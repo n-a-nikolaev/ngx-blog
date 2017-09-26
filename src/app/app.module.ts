9e4c8ef480fc1c13744f0f9471a5b69b811d0825
Nikolay Nikolaev <nikolaev.a.nikolay@gmail.com>
2017-09-26 22:20:11 +0300

    Add angular structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app-material.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { AdminModule } from './admin/admin.module';
+import { PublicModule } from './public/public.module';
+import { AdminRoutingModule } from './admin/admin-routing.module';
+import { PublicRoutingModule } from './public/public-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    SharedModule,
+    AdminModule,
+    PublicModule,
+    AppMaterialModule,
+    AdminRoutingModule,
+    PublicRoutingModule,
     AppRoutingModule
   ],
   providers: [],
